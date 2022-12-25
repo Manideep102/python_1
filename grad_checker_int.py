d75e5018b51768a2e943aea5721e83410c981aae
Manideep <saimanideep@gmail.com>
2022-12-25 07:11:19 +0530

    Add files via upload

--- grad_checker_int.py
+++ grad_checker_int.py
@@ -1,6 +1,4 @@
-# GRADED FUNCTION: gradient_check_n
-
-def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7, print_msg=False):
+def gradient_check_n(parameters, gradients, X, Y, epsilon = 1e-7, print_msg=False):
     """
     Checks if backward_propagation_n computes correctly the gradient of the cost output by forward_propagation_n
     
@@ -17,7 +15,6 @@ def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7, print_msg=False)
     
     # Set-up variables
     parameters_values, _ = dictionary_to_vector(parameters)
-    
     grad = gradients_to_vector(gradients)
     num_parameters = parameters_values.shape[0]
     J_plus = np.zeros((num_parameters, 1))
@@ -29,47 +26,34 @@ def gradient_check_n(parameters, gradients, X, Y, epsilon=1e-7, print_msg=False)
         
         # Compute J_plus[i]. Inputs: "parameters_values, epsilon". Output = "J_plus[i]".
         # "_" is used because the function you have to outputs two parameters but we only care about the first one
-        #(approx. 3 lines)
-        theta_plus =  np.copy(parameters_values)                                      # Step 1
-        theta_plus[i][0] = theta_plus[i][0] + epsilon                                    # Step 2
-        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(theta_plus))                                     # Step 3
-        # YOUR CODE STARTS HERE
-        
-        
-        # YOUR CODE ENDS HERE
+        ### START CODE HERE ### (approx. 3 lines)
+        thetaplus = np.copy(parameters_values)                                      # Step 1
+        thetaplus[i][0] = thetaplus[i][0] + epsilon                                # Step 2
+        J_plus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaplus))  # Step 3
+        ### END CODE HERE ###
         
         # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
-        #(approx. 3 lines)
-        theta_minus =   np.copy(parameters_values)  
-        print(theta_minus[i][0] + epsilon) 
-        theta_minus[i][0] = theta_minus[i][0] + epsilon                                  # Step 2        
-        J_minus[i], _ =  forward_propagation_n(X, Y, vector_to_dictionary(theta_minus))                                # Step 3
-        # YOUR CODE STARTS HERE
-        
-        
-        # YOUR CODE ENDS HERE
+        ### START CODE HERE ### (approx. 3 lines)
+        thetaminus = np.copy(parameters_values)                                     # Step 1
+        thetaminus[i][0] = thetaminus[i][0] - epsilon                               # Step 2        
+        J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaminus))                                  # Step 3
+        ### END CODE HERE ###
         
         # Compute gradapprox[i]
-        # (approx. 1 line)
-        gradapprox[i] = (J_plus[i] - J_minus[i])/ (2 * epsilon) 
-        # YOUR CODE STARTS HERE
-        
-        
-        # YOUR CODE ENDS HERE
+        ### START CODE HERE ### (approx. 1 line)
+        gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
+        ### END CODE HERE ###
     
     # Compare gradapprox to backward propagation gradients by computing difference.
-    # (approx. 3 line)
-    numerator =   np.linalg.norm(grad - gradapprox)                              # Step 1'
-    denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox )                              # Step 2'
-    difference =  np.divide(numerator, denominator)                                       # Step 3'
-    # YOUR CODE STARTS HERE
-    
-    
-    # YOUR CODE ENDS HERE
-    if print_msg:
-        if difference > 2e-7:
-            print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
-        else:
-            print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+    ### START CODE HERE ### (approx. 1 line)
+    numerator = np.linalg.norm(grad - gradapprox) # Step 1'
+    denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox)                                         # Step 2'
+    difference = numerator / denominator                                          # Step 3'
+    ### END CODE HERE ###
 
+    if difference > 1e-7:
+        print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+    else:
+        print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+    
     return difference
